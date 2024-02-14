1b9bbf6c2f44168efe54ee39efb08b066cf22593
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-02-14 19:31:16 -0300

    Middleware para verificar datos de formulario de nueva reparacion

--- src/routes/nuevareparacion.js
+++ src/routes/nuevareparacion.js
@@ -1,4 +1,3 @@
-// usersRoutes.js
 const express = require('express');
 const router = express.Router();
 const path = require('path');
@@ -6,12 +5,23 @@ const Db = require('../controllers/pg.js');
 
 let pg = Db.getInstance();
 
-// Define tus rutas para usuarios
+// Middleware para validar los datos de req.body
+function validarReqBody(req, res, next) {
+    const datosObligatorios = [ req.body.dni, req.body.falla, 
+        req.body.tipo, req.body.fabricante, req.body.modelo ];
+
+    datosObligatorios.forEach(d => {
+        if(!d)
+            return res.status(400).json({ error: 'Faltan campos obligatorios en req.body' , datosRecibidos: datosObligatorios}); 
+    });
+    next();
+  }
+
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname
         , '../../public/views/ingreso_producto.html'))
 })
-.post('/', (req, res) => {
+.post('/', validarReqBody, (req, res) => {
     pg.nuevaReparacion(req.body);
     res.json({nombre: req.body.nombre, monto: 10000});
 })
