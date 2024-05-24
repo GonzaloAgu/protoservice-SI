8a687a6dddde69c6349fb29801b061833f90399e
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-05-24 01:25:48 -0300

    removida importacion innecesaria

--- src/routes/tipos_electro.js
+++ src/routes/tipos_electro.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Db = require('../controllers/pg.js');
 const { TipoElectrodomestico, Fabricante } = require('../models/models');
-
-const db = Db.getInstance();
 
 
 router.get('/', async(req, res) => {
