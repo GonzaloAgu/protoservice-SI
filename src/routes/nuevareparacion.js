3cab0732e8b6906a3bbf1c9d41f912ec743d1a54
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-05-24 01:29:41 -0300

    removida funcion innecesaria

--- src/routes/nuevareparacion.js
+++ src/routes/nuevareparacion.js
@@ -4,18 +4,6 @@ const path = require('path');
 const { Cliente } = require('../models/models');
 
 
-// Middleware para validar los datos de req.body
-function validarReqBody(req, res, next) {
-    const datosObligatorios = [ req.body.dni, req.body.falla, 
-        req.body.tipo, req.body.fabricante, req.body.modelo ];
-
-    datosObligatorios.forEach(d => {
-        if(!d)
-            return res.status(400).json({ error: 'Faltan campos obligatorios en req.body' , datosRecibidos: datosObligatorios}); 
-    });
-    next();
-  }
-
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname
         , '../../public/views/ingreso_producto.html'))
