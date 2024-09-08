d4de0a5e1684a008715c2df5fda9603c7deb9696
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-09-07 21:36:43 -0300

    mejores nombres para los archivos html

--- src/routes.js
+++ src/routes.js
@@ -44,54 +44,15 @@ router.get('/', (req, res) => {
 });
 
 router.get('/consulta', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/views/consultas.html'));
+    res.sendFile(path.join(__dirname, '../public/views/buscar_reparaciones.html'));
 });
 
 router.get('/nuevareparacion', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/views/ingreso_producto.html'));
+    res.sendFile(path.join(__dirname, '../public/views/ingresar_reparacion.html'));
 });
 
 router.get('/reparacion', async (req, res) => {
-    try {
-        let rep = new Reparacion(req.query.id);
-        if (await rep.obtener()) {
-            const [cliente, electrodomestico] = await Promise.all([
-                rep.getClienteObj(),
-                rep.getElectrodomesticoObj()
-            ]);
-            const fabricante = await electrodomestico.getFabricanteObj();
-            const mediosPago = await MedioPago.obtenerTodos();
-            const factura = await rep.getFacturaObj();
-
-            if (!cliente || !electrodomestico || !fabricante) {
-                return res.status(500).send(`
-                    <h1>Error 500</h1>
-                    <p>Hubo un error al obtener la información del cliente, electrodoméstico o fabricante.</p>
-                    <a href="/">Volver a la página principal.</a>
-                `);
-            }
-
-            return res.render(path.join(__dirname, '../../public/views/reparacionView.ejs'), {
-                reparacion: rep,
-                cliente,
-                electrodomestico,
-                fabricante,
-                mediosPago,
-                factura
-            });
-        } else {
-            return res.status(404).send(`
-                <h1>Error 404: Reparación no encontrada.</h1>
-                <a href="/">Volver a la página principal.</a>
-            `);
-        }
-    } catch (error) {
-        return res.status(500).send(`
-            <h1>Error 500: Error interno del servidor.</h1>
-            <p>${error.message}</p>
-            <a href="/">Volver a la página principal.</a>
-        `);
-    }
+    return res.sendFile(path.join(__dirname, '../public/views/reparacion.html'));
 });
 
 module.exports = router;
