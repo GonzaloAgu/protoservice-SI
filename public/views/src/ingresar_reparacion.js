0c399f2b8d0e0c2f5a2f1ea1680a5a683f7d8e03
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-09-17 15:00:55 -0300

    [interfaz] agregar tipos y marcas en formulario de ingreso

--- public/views/src/ingresar_reparacion.js
+++ public/views/src/ingresar_reparacion.js
@@ -83,33 +83,7 @@ let clienteState = {
   modified: false
 }
 
-
-function main() {
-
-  $('#nombre-cliente').focus()
-
-  $(() => {
-    // Se obtienen los clientes para el autocompletado.
-    fetch('api/cliente')
-      .then(res => res.json())
-      .then(data => {
-        clientes = data;
-        const nombresClientes = data.map(cliente => cliente.nombre);
-        $("#nombre-cliente").autocomplete({
-          source: nombresClientes,
-          minLength: 2,
-          open: function (event, ui) {
-            $('.ui-autocomplete').addClass('dropdown-menu bg-dark text-light');
-            $('.ui-menu-item').addClass('dropdown-item');
-          }
-        });
-      }
-      )
-      .catch(error => console.error(error))
-  });
-
-  $('#nombre-cliente').on('keydown', (event) => {
-
+const onAutocomplete = event => {
     clienteState.exists = clienteState.modified = false;
 
     if (event.keyCode == 8) { // backspace
@@ -140,9 +114,105 @@ function main() {
       });
 
       // Si el cliente no existe, ingresamos su telefono
-      if (!clientExist) document.getElementById('telefono-cliente').focus();;
+      if (!clientExist) document.getElementById('telefono-cliente').focus();
     }
-  })
+}
+
+const submitForm = async (event) => {
+  event.preventDefault();
+  $('#loading-spinner').css('display', 'block');
+
+  let form = {};
+
+  try {
+    if (clienteState.exists && clienteState.modified) {
+
+      actualizarCliente({
+        id: cliente_id,
+        telefono: $('#telefono-cliente').val()
+      });
+
+    }
+
+    if (!clienteState.exists) {
+      cliente_id = await agregarCliente({
+        nombre: $('#nombre-cliente').val(),
+        telefono: $('#telefono-cliente').val()
+      })
+      form.id_cliente = cliente_id;
+    }
+
+    form = {
+      id_cliente: cliente_id,
+      tipo_electro_id: $('#tipo-input').val(),
+      fabricante_id: $('#fabricante-input').val(),
+      modelo_electro: $('#modelo-input').val(),
+      desc_falla: $('#desc-fallo').val()
+    }
+    const response = await fetch('/api/reparacion', {
+      body: JSON.stringify(form),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    })
+
+    data = await response.json();
+
+    if(!data || !data.reparacion_id) throw new Error('No se pudo guardar en la base de datos.')
+
+    $('#successPopup')
+      .removeClass('alert-danger')
+      .addClass('alert-success')
+      .addClass('show')
+      .text(`La reparación ${data.reparacion_id} ha sido agregada con éxito.`);
+
+    $('#submit').attr('disabled', '');
+
+    setTimeout(() => {
+      window.location.href = "/reparacion?id=" + data.reparacion_id;
+    }, 1000);
+  } catch (error) {
+    $('#successPopup')
+      .addClass('show')
+      .removeClass('alert-success')
+      .addClass('alert-danger')
+      .html(`<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Se produjo un error al agregar la reparación: ${error.message}`);
+    console.log(error)
+  } finally {
+    $('#loading-spinner').css('display', 'none');
+    setTimeout(() => $('#successPopup').removeClass('show'), 5000);
+  }
+
+}
+
+function main() {
+
+  $('#nombre-cliente').focus()
+
+  $(() => {
+    // Se obtienen los clientes para el autocompletado.
+    fetch('api/cliente')
+      .then(res => res.json())
+      .then(data => {
+        clientes = data;
+        const nombresClientes = data.map(cliente => cliente.nombre);
+        $("#nombre-cliente").autocomplete({
+          source: nombresClientes,
+          minLength: 2,
+          open: function (event, ui) {
+            $('.ui-autocomplete').addClass('dropdown-menu bg-dark text-light');
+            $('.ui-menu-item').addClass('dropdown-item');
+          }
+        });
+        return;
+      }
+      )
+      .then(() => $('.ui-menu-item-wrapper').on('click', onAutocomplete))
+      .catch(error => console.error(error))
+  });
+
+  $('#nombre-cliente').on('keydown', onAutocomplete)
 
   $('#telefono-cliente').on('keyup', event => {
     event.preventDefault();
@@ -152,73 +222,7 @@ function main() {
     }
   })
 
-  $('#reparacion-form').on('submit', async (event) => {
-    event.preventDefault();
-    $('#loading-spinner').css('display', 'block');
-
-    let form = {};
-
-    try {
-      if (clienteState.exists && clienteState.modified) {
-
-        actualizarCliente({
-          id: cliente_id,
-          telefono: $('#telefono-cliente').val()
-        });
-
-      }
-
-      if (!clienteState.exists) {
-        cliente_id = await agregarCliente({
-          nombre: $('#nombre-cliente').val(),
-          telefono: $('#telefono-cliente').val()
-        })
-        form.id_cliente = cliente_id;
-      }
-
-      form = {
-        id_cliente: cliente_id,
-        tipo_electro_id: $('#tipo-input').val(),
-        fabricante_id: $('#fabricante-input').val(),
-        modelo_electro: $('#modelo-input').val(),
-        desc_falla: $('#desc-fallo').val()
-      }
-      const response = await fetch('/api/reparacion', {
-        body: JSON.stringify(form),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        method: 'POST'
-      })
-
-      data = await response.json();
-
-      if(!data || !data.reparacion_id) throw new Error('No se pudo guardar en la base de datos.')
-
-      $('#successPopup')
-        .removeClass('alert-danger')
-        .addClass('alert-success')
-        .addClass('show')
-        .text(`La reparación ${data.reparacion_id} ha sido agregada con éxito.`);
-
-      $('#submit').attr('disabled', '');
-
-      setTimeout(() => {
-        window.location.href = "/reparacion?id=" + data.reparacion_id;
-      }, 1000);
-    } catch (error) {
-      $('#successPopup')
-        .addClass('show')
-        .removeClass('alert-success')
-        .addClass('alert-danger')
-        .html(`<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Se produjo un error al agregar la reparación: ${error.message}`);
-      console.log(error)
-    } finally {
-      $('#loading-spinner').css('display', 'none');
-      setTimeout(() => $('#successPopup').removeClass('show'), 5000);
-    }
-
-  })
+  $('#reparacion-form').on('submit', submitForm);
 }
 
 document.addEventListener('DOMContentLoaded', main);
